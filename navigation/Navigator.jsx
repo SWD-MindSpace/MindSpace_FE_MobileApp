323ff686293c03685bd23ddd33bda27207601e85
QuanLM270302 <leminhquan270305@gmail.com>
2025-02-12 08:30:23 +0700

    feat(auth): add login page with no functionality

--- navigation/Navigator.jsx
+++ navigation/Navigator.jsx
@@ -1,12 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import React from 'react'
-
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import AppWelcomeLogo from '../screens/AppWelcomeLogo';
+import HomeScreen from '../screens/HomeScreen';
+import MainScreen from '../screens/MainScreen';
 const Navigator = () => {
-  return (
-    <View>
-      <Text>Navigator</Text>
-    </View>
-  )
+    const Stack = createNativeStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name='AppWelcome' component={AppWelcomeLogo} />
+            <Stack.Screen name='HomeScreen' component={HomeScreen} />
+            <Stack.Screen name='MainScreen' component={MainScreen} />
+        </Stack.Navigator>
+    )
 }
 
 export default Navigator
