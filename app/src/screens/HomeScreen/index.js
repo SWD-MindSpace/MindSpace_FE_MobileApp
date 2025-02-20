8d361aac05ac27edc17ed65f27f398f5822ec6bc
QuanLM270302 <leminhquan270305@gmail.com>
2025-02-20 11:19:46 +0700

    resolve conflict

--- app/src/screens/HomeScreen/index.js
+++ app/src/screens/HomeScreen/index.js
@@ -4,10 +4,10 @@ import { AuthContext } from '@/app/src/context/AuthContext';
 
 const HomeScreen = ({ navigation }) => {
     // const apiURL = "https://192.168.101.2:7096/api/v1/identity/login";
-    const apiURL = "https://localhost:7096/api/v1/identity/login"
+    // const apiURL = "https://localhost:7096/api/v1/identity/login"
     // const apiURL = "http://localhost:8010/proxy/api/v1/identity/login";
     // const apiURL = "https://127.0.0.1:7096/api/v1/identity/login"
-
+    const apiURL = "http://10.0.2.2:5021/api/v1/identity/login";
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [rememberMe, setRememberMe] = useState(false);
